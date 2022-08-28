dab2b3e9094bd14ac02069e3a408462dea0d1b98
R K Ganeshan <61367501+rkganeshan@users.noreply.github.com>
2022-08-28 13:55:49 +0530

    added get route on "/" to serve the apiDocs file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const cors=require("cors");
-
+const fs=require("fs");
 
 //configurations
 const app=express();
@@ -12,6 +12,18 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+//api documentation
+app.get("/",(req,res)=>{
+    fs.readFile("./apiDocs.json",(err,data)=>{
+        if(err)
+        {
+            res.status(400).json({error:err})
+        }
+        const docs=JSON.parse(data);
+        res.status(200).json(docs)
+    })
+})
 
 //bringing routes
 const productRoutes=require("./routes/product");
